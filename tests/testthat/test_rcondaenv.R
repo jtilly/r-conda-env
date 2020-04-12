66f5a1c9e9740e3b0c80f0c55ff68f18a04a19ae
Jan Tilly <jantilly@gmail.com>
2020-04-12 22:44:29 +0200

    Add tests.

--- tests/testthat/test_rcondaenv.R
+++ tests/testthat/test_rcondaenv.R
@@ -1,8 +1,36 @@
 test_that("readme code works", {
   create_package_env()
   df <- tibble::tribble(~x, ~y, ~z, "a", 2, 3.6, "b", 1, 8.5)
+
   expect_equal(python_model_predict(df), c(0, 1))
-  expect_equal(check_pandas_version(), "The installed Pandas version is 1.0.3")
+  expect_equal(
+    check_pandas_version(),
+    "The installed Pandas version is 1.0.3"
+  )
+
+  stop_cluster()
+  expect_equal(python_model_predict(df), c(0, 1))
+  expect_equal(
+    check_pandas_version(),
+    "The installed Pandas version is 1.0.3"
+  )
+
+  create_cluster(force = TRUE)
+  expect_equal(python_model_predict(df), c(0, 1))
+  expect_equal(
+    check_pandas_version(),
+    "The installed Pandas version is 1.0.3"
+  )
+
+  if (.Platform$OS.type == "unix") {
+    set_cluster_type("FORK")
+    expect_equal(python_model_predict(df), c(0, 1))
+    expect_equal(
+      check_pandas_version(),
+      "The installed Pandas version is 1.0.3"
+    )
+    set_cluster_type("PSOCK")
+  }
 })
 
 test_that("test that we can switch python versions", {
@@ -12,7 +40,10 @@ test_that("test that we can switch python versions", {
   version_orig <- sys$version
 
   # make sure that we can still access this package's conda environment
-  expect_equal(check_pandas_version(), "The installed Pandas version is 1.0.3")
+  expect_equal(
+    check_pandas_version(),
+    "The installed Pandas version is 1.0.3"
+  )
 
   # use reticulate and run some python code in the base environment
   reticulate::use_condaenv("base", conda = get_conda_path())
